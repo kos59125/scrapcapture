71c8d6e96d659aae61d92bb26adbd06107e9ba47
kos59125 <kos59125@localhost>
2009-04-16 03:23:37 +0000

    OS ?????? DWM ??????????????????????????????????

--- WindowCapture/Program.cs
+++ WindowCapture/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Deployment.Application;
 
 namespace RecycleBin.WindowCapture
 {
@@ -13,9 +14,36 @@ namespace RecycleBin.WindowCapture
 		[STAThread]
 		static void Main()
 		{
+			try
+			{
+				ValidatePlatform();
+				LaunchApplication();
+			}
+			catch (PlatformNotSupportedException ex)
+			{
+				MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Application.Exit();
+			}
+		}
+
+		private static void LaunchApplication()
+		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new WindowCaptureForm());
+		}
+
+		private static void ValidatePlatform()
+		{
+			OperatingSystem os = Environment.OSVersion;
+			if (os.Platform != PlatformID.Win32NT || os.Version.Major < 6 /* less than Vista */)
+			{
+				throw new PlatformNotSupportedException("Windows Vista 以降の OS でのみ動作します。");
+			}
+			if (!DesktopWindowManager.IsCompositionEnabled)
+			{
+				throw new ApplicationException("DWM コンポジションが有効ではありません。");
+			}
 		}
 	}
 }
