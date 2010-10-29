1e17ccf1022aa93da851b630c1698a14308fd417
kos59125 <kos59125@localhost>
2010-10-29 17:17:35 +0900

    ????????????????? ????????

--- WPF/App.xaml.cs
+++ WPF/App.xaml.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Windows;
 
-namespace WPF
+namespace RecycleBin.ScrapCapture
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application
 	{
+		private static readonly Version WindowsVistaVersion = new Version(6, 0);
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			OperatingSystem os = Environment.OSVersion;
+			if (os.Platform != PlatformID.Win32NT || os.Version < WindowsVistaVersion)
+			{
+				MessageBox.Show("Windows Vista 以降の Windows Aero に対応した OS で動作します。", "未対応 OS", MessageBoxButton.OK, MessageBoxImage.Error);
+				Current.Shutdown();
+			}
+			if (!DesktopWindowManager.IsCompositionEnabled)
+			{
+				MessageBox.Show("デスクトップ コンポジションが無効です。", "DWM エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+				Current.Shutdown();
+			}
+		}
 	}
 }
